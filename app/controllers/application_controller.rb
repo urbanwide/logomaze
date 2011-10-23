94c7e005c5a9407debfb7d66a97e588255125257
Rob Lee <robl+git@rjlee.net>
2011-10-23 14:48:07 +0100

    Tidied naming

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :set_title
+  
+  protected
+  def set_title
+    @title = "Escape the maze"
+  end
+  
 end
